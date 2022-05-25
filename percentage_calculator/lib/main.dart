d1b6fd48c80c45ae7b429f7931d28f78db00659f
ABDULLAH IBNA MUKTA <56233279+muktabd@users.noreply.github.com>
2022-05-25 20:37:22 +0800

    upgrade the code

--- percentage_calculator/lib/main.dart
+++ percentage_calculator/lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,14 +20,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final numAmountController = TextEditingController(text: '0.0');
-  final numPercentageController = TextEditingController(text: '0.0');
+  final numAmountController = TextEditingController();
+  final numPercentageController = TextEditingController();
 
   bool validate = false;
   @override
@@ -36,128 +41,125 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[300],
-          title: Text(('Percentage Calculator')),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[300],
+        title: Text(('Percentage Calculator')),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Colors.blue.shade300,
+            Colors.blue.shade200,
+            Colors.blue.shade100,
+            Colors.blue.shade50,
+          ]),
         ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-              Colors.blue[300],
-              Colors.blue[200],
-              Colors.blue[100],
-              Colors.blue[50],
-            ]),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                    padding: EdgeInsets.all(5.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: numAmountController,
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'AMOUNT *',
-                            errorText: validate ? 'Fill the field' : null,
-                            errorStyle: TextStyle(
-                              color: Colors.red,
-                            ),
-                            labelStyle: TextStyle(color: Colors.white),
-                          ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: numAmountController,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'AMOUNT *',
+                        errorText: validate ? 'Fill the field' : null,
+                        errorStyle: const TextStyle(
+                          color: Colors.red,
                         ),
-                        TextField(
-                          controller: numPercentageController,
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Percantage*',
-                            errorText: validate ? 'Fill the field' : null,
-                            errorStyle: TextStyle(
-                              color: Colors.red,
-                            ),
-                            labelStyle: TextStyle(color: Colors.white),
-                          ),
+                        labelStyle: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextField(
+                      controller: numPercentageController,
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Percantage*',
+                        errorText: validate ? 'Fill the field' : null,
+                        errorStyle: TextStyle(
+                          color: Colors.red,
                         ),
-                        Container(
-                          height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.orange[900]),
-                          child: RaisedButton(
-                            color: Colors.blueAccent,
-                            child: Text(
-                              "SEE THE RESULT",
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onPressed: () {
-                              //register navigation is here
-                              calculateNow();
-                            },
-                          ),
-                        ),
-                        Text(
-                          'Percentage is : $displayResult',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
-                        ),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          calculateNow();
+                        });
 
-                        Text(
-                          'After minus the percentage, amount is : $displayResultMinus',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
+                        //
+                      },
+                      child: Text(
+                        "SEE THE RESULT",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-
-                        Text(
-                          'After adding the percentage, amount is : $displayResultAdd',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
-                        ),
-                     
-                        //Text('$inputtedAmount'),
-                      ],
-                    ))
-              ],
-            ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Percentage is : $displayResult',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'After minus the percentage, amount is : $displayResultMinus',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'After adding the percentage, amount is : $displayResultAdd',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
-  var displayResult = 0.0, inputtedAmount = 0.0, 
-  inputtedPercentage = 0.0, displayResultAdd = 0.0, displayResultMinus=0.0;
+  var displayResult = 0.0,
+      inputtedAmount = 0.0,
+      inputtedPercentage = 0.0,
+      displayResultAdd = 0.0,
+      displayResultMinus = 0.0;
 
   void calculateNow() {
     setState(() {
       inputtedAmount = double.parse(numAmountController.text);
       inputtedPercentage = double.parse(numPercentageController.text);
 
-      displayResult = (inputtedAmount * inputtedPercentage)/100;
+      displayResult = (inputtedAmount * inputtedPercentage) / 100;
 
-      
-      displayResultAdd =  inputtedAmount + displayResult;
+      displayResultAdd = inputtedAmount + displayResult;
       displayResultMinus = inputtedAmount - displayResult;
-      
     });
-
-
   }
 }
